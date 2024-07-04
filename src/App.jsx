59f13811913951cfcc668588b8349dc7e7c3dd30
mk467 <79553615+mk467@users.noreply.github.com>
2024-07-04 19:13:45 +0100

    eee

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./style.css";
+
+
+
+function App() {
+return(
+<>
+  <div style={{backgroundColor:"purple",height:"100vh",width:"100%",marginTop:"0%"}}>
+<div style={{}}>
+    ffffffffffff
+</div>
+  </div>
+</>
+
+
+
+);
+
+}
+
+export default App
