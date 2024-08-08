4b15d83e3ebb4ee1f5598b5993327934def35edc
mk467 <79553615+mk467@users.noreply.github.com>
2024-08-08 18:32:48 +0100

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./style.css";
 import { Footer } from "./Components/Footer";
 import { NavBar } from "./Components/NavBar";
 import { Home } from "./Components/Home";
+import { Canvas } from "./Components/Canvas";
+import { Customizer } from "./Components/Customizer";
 
 
 
@@ -11,14 +12,12 @@ import { Home } from "./Components/Home";
 function App() {
 return(
 <>
-  <BrowserRouter>
-  <NavBar/>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="*" element={<Home />} />
-  </Routes>
-  <Footer/>
-  </BrowserRouter>
+<main className="app transition-all ease-in">
+<Home/>
+<Canvas/>
+<Customizer/>
+
+</main>
 </>
 
 
@@ -28,3 +27,13 @@ return(
 }
 
 export default App
+/*
+  <BrowserRouter>
+  <NavBar/>
+  <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="*" element={<Home />} />
+  </Routes>
+  <Footer/>
+  </BrowserRouter>
+  */
