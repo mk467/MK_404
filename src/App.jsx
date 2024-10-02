cbadf0778a89870bd81c32c5b31174a9e7c82d8e
mk467 <79553615+mk467@users.noreply.github.com>
2024-10-03 00:07:31 +0100

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,16 @@
-
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { NavBar } from "./Components/NavBar";
-import { Bannertop } from "./Components/Bannertop";
-import { Login } from "./Components/Login";
-import { Login } from "./Components/Register";
-
-
-
+import { Home } from "./Components/Home";
 
 function App() {
-return(
-<>
-
-<BrowserRouter>
-  <NavBar/>
-  <Routes>
-    <Route path="/Login" element={<Login/>} />
-    <Route path="/Register" element={<Register/>} />
-  </Routes>
-</BrowserRouter>
-</>
-
-
-
-);
-
+  return (
+    <>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
-/*
-  <BrowserRouter>
-  <NavBar/>
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="*" element={<Home />} />
-  </Routes>
-  <Footer/>
-  </BrowserRouter>
-  */
+export default App;
--- src/Customizer.jsx
+++
@@ -1,8 +0,0 @@
-{/* Download button */}
-            <button className='download-btn' onClick={downloadCanvasToImage}>
-              <img
-                src={download}
-                alt='download_image'
-                className='w-3/5 h-3/5 object-contain'
-              />
-            </button>
